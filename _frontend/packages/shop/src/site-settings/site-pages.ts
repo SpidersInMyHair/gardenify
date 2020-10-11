c24c5748f5b39b3aa65ee748a50b9e77ca5a5757
amirhar <amirhar98@gmail.com>
2020-10-12 00:15:03 +1100

    Frontend Init

---
+++ _frontend/packages/shop/src/site-settings/site-pages.ts
@@ -0,0 +1,11 @@
+import banner from 'assets/images/banner/banner.png';
+
+export const sitePages = {
+  grocery: {
+    page_title: 'Gardenify',
+    page_description: 'Search our index of flora for all your growing and cultivating needs',
+    banner_title_id: 'home.title',
+    banner_description_id: 'home.subtitle',
+    banner_image_url: banner,
+  },
+};
