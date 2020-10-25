f0a47c86f8f3b2e5296cd9273f3a17236be1de1a
Forrest <forrest.c.koch@gmail.com>
2020-10-25 22:18:34 +1100

    add offset parameter

--- _backend/plant_service/server.ts
+++ _backend/plant_service/server.ts
@@ -56,7 +56,7 @@ app.get(`${SERVICE}`, (req: any, res: GetPlantsResponse) => {
     offset = req.query.offset;
   }  
   //console.log(req.query)
-  repo.getPlants(0,limit)
+  repo.getPlants(offset,limit)
   .then((plantVarieties: Array<PlantVariety>) => {
     res.send(plantVarieties).status(200).end();
   })
