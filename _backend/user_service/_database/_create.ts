6fc084b21ca1be1d1b4c2b4eb6465659c7f5d858
11XGX <ellisn11@gmail.com>
2020-11-01 01:09:21 +1100

    Subtle changes to user_service database files for convenience

--- _backend/user_service/_database/_create.ts
+++ _backend/user_service/_database/_create.ts
@@ -3,22 +3,22 @@ module.exports = express();
 
 connection = require('../../../_repository/_config').connection;
 
-connection.query(`                                                    \
-  DROP TABLE IF EXISTS sessions;                                      \    
-  DROP TABLE IF EXISTS users;                                         \
-  CREATE TABLE IF NOT EXISTS users (                                  \
-    _id         int           NOT NULL   AUTO_INCREMENT,              \
-    id          char(12)      UNIQUE     NOT NULL,                    \
-    email       varchar(64)   UNIQUE     NOT NULL,                    \
-    password    char(64)      NOT NULL,                               \
-    PRIMARY KEY (_id)                                                 \
-  );                                                                  \
-  CREATE TABLE IF NOT EXISTS sessions (                               \
-    user_id     int           NOT NULL,                               \
-    session_key char(64),                                             \
-    FOREIGN KEY (user_id) REFERENCES users(_id)                       \
+connection.query(` \
+  DROP TABLE IF EXISTS sessions; \    
+  DROP TABLE IF EXISTS users; \
+  CREATE TABLE IF NOT EXISTS users ( \
+    _id         int           NOT NULL   AUTO_INCREMENT, \
+    id          char(12)      UNIQUE     NOT NULL, \
+    email       varchar(64)   UNIQUE     NOT NULL, \
+    password    char(64)      NOT NULL,  \
+    PRIMARY KEY (_id) \
+  ); \
+  CREATE TABLE IF NOT EXISTS sessions ( \
+    user_id     int           NOT NULL, \
+    session_key char(64), \
+    FOREIGN KEY (user_id) REFERENCES users(_id) \
   );`
-, (err: any) => {
-  if (err) throw err;
-  console.log('> MySQL: Created user tables');
-});
+  , (err: any) => {
+    if (err) throw err;
+    console.log('> MySQL: Created user tables');
+  });
--- _backend/user_service/_database/_init.ts
+++ _backend/user_service/_database/_init.ts
@@ -3,14 +3,14 @@ module.exports = express();
 
 require('../../../_repository/_config').connection;
 
-connection.query(`                                                            \
-  INSERT INTO users (id, email, password)                                     \
-  VALUES (                                                                    \
-    "root",                                                                   \
-    "root@gardenify.com",                                                     \
-    "aa1d14e95d68f8c2d5539450318d42f56ab54e396018c4197cfc88d941954cb8"        \
+connection.query(` \
+  INSERT INTO users (id, email, password) \
+  VALUES ( \
+    "root", \
+    "root@gardenify.com", \
+    "aa1d14e95d68f8c2d5539450318d42f56ab54e396018c4197cfc88d941954cb8" \
   );`
-, (err: any) => {
-  if (err) throw err;
-  console.log('> MySQL: Initialized users tables');
-});
+  , (err: any) => {
+    if (err) throw err;
+    console.log('> MySQL: Initialized users tables');
+  });
