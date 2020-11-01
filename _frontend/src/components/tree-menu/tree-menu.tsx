746a15a194240323f85f052c1f76291054f952fb
amirhar <amirhar98@gmail.com>
2020-11-01 20:32:49 +1100

    Advanced Search

--- _frontend/src/components/tree-menu/tree-menu.tsx
+++ _frontend/src/components/tree-menu/tree-menu.tsx
@@ -1,35 +1,57 @@
-import React, { useState, useEffect } from 'react';
-import { usePrevious, useMeasure } from 'utils/hooks';
-import { useSpring, animated } from 'react-spring';
-import { Frame, Title, Content, Header, IconWrapper } from './tree-menu.style';
+import React from 'react';
+import { Formik, Form } from 'formik';
+import { useRouter } from 'next/router';
 
-import * as icons from 'assets/icons/category-icons';
 import { Input } from 'components/forms/input';
 import { Button } from 'components/button/button';
 
-const onSearch = (event: any) => {
-  event.preventDefault();
-  console.log('hello');
+const initialValues = {
+  name:         {title: 'Name',         value: ''},
+  common_name:  {title: 'Common Name',  value: ''},
+  genus:        {title: 'Genus',        value: ''},
+  family:       {title: 'Family',      value: ''}
 }
 
 export const TreeMenu: React.FC = () => {
+
+  const router = useRouter();
+
+  // if (error) return <ErrorMessage message={error.message} />;
+  const { pathname, query } = router;
+
+  const onSubmit = (values: any) => {
+    const queryValues = {}
+    for (const property in values) {
+      const val = values[property].value
+      if (val && val !== '') queryValues[property] = val
+    }
+
+    router.push({
+      pathname,
+      query: { ...query, ...queryValues },
+    })
+  };
+
   return (
-    <>
-      <form onSubmit={onSearch}>
+    <Formik 
+      onSubmit={onSubmit}
+      initialValues={initialValues} 
+    >
+      {({
+        values,
+        setFieldValue
+      }) => (
+      <Form >
         {
-          [
-            { title: 'Name', name: 'name' },
-            { title: 'Common Name', name: 'common_name' },
-            { title: 'Genus', name: 'genus' },
-            { title: 'Family', name: 'family' }
-          ].map((category) => {
+          Object.keys(initialValues).map((category) => {
             return (
-              <Input id={category.name} placeholder={category.title} />
+              <Input onChange={e => setFieldValue(category, {...values[category], value: e.target.value})} value={values[category].value} name={category} key={category} placeholder={values[category].title} />
             );
           })
         }
         <Button type="submit">Search</Button>
-      </form>
-    </>
+      </Form>
+      )}
+    </Formik>
   );
 };
