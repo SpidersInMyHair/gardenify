46e9bb74efb02e4620de70e9fbdc28fe305ed174
11XGX <ellisn11@gmail.com>
2020-11-08 15:19:12 +1100

    Random indenting fixes from ESLint

--- _backend/user_service/server.ts
+++ _backend/user_service/server.ts
@@ -23,7 +23,7 @@ import {
   UpdateUserProfileRequest,
   UpdateUserProfileResponse
 } from "./_messages";
-import {Profile, User} from "../../protos/_backend/user_service/protos/user_pb";
+import { Profile, User } from "../../protos/_backend/user_service/protos/user_pb";
 
 /* --------------------------- SERVICE ENDPOINTS --------------------------- */
 // GET  /user               Get if user session is valid by examining cookies.
@@ -44,14 +44,14 @@ app.get(`${SERVICE}`, (req: GetUserRequest, res: GetUserResponse) => {
   }
 
   repo.getSession(id, session_key)
-  .then((session) => {
-    if (!session) res.sendStatus(403);
-    else res.sendStatus(200);
-  })
-  .catch((err: any) => {
-    console.log(err);
-    res.sendStatus(500);
-  })
+    .then((session) => {
+      if (!session) res.sendStatus(403);
+      else res.sendStatus(200);
+    })
+    .catch((err: any) => {
+      console.log(err);
+      res.sendStatus(500);
+    })
 });
 
 // POST /user               Create a new user.
@@ -61,21 +61,21 @@ app.post(`${SERVICE}`, (req: CreateUserRequest, res: CreateUserResponse) => {
   const password: string = sha256(req.body.password);
 
   repo.createUser(id, email, password)
-  .then(() => res.sendStatus(200))
-  .catch((err: any) => {
-    console.log(err);
-    res.sendStatus(500);
-  });
+    .then(() => res.sendStatus(200))
+    .catch((err: any) => {
+      console.log(err);
+      res.sendStatus(500);
+    });
 });
 
 // GET  /user/:id           Get the user with the given id.
 app.get(`${SERVICE}/:id`, (req: GetUserByIdRequest, res: GetUserByIdResponse) => {
   repo.getUserById(req.params.id)
-  .then((user: User) => res.send(user).status(200).end())
-  .catch((err: any) => {
-    console.log(err);
-    res.sendStatus(500);
-  });
+    .then((user: User) => res.send(user).status(200).end())
+    .catch((err: any) => {
+      console.log(err);
+      res.sendStatus(500);
+    });
 });
 
 // POST /user/login         Login an user.
@@ -84,16 +84,16 @@ app.post(`${SERVICE}/login`, (req: LoginUserRequest, res: LoginUserResponse) => 
   const password: string = sha256(req.body.password);
 
   repo.getUser(email, password)
-  .then((user: User) => {
-    if (!user) res.status(403).end();
-    const session_key: string = "CHANGE_THIS_TO_BE_GENERATED";
-    repo.setSession(user.getId(), session_key)
-    .then(() => res.cookie('UID', user.getId()).cookie('SID', session_key).send(user).status(200).end())
-  })
-  .catch((err: any) => {
-    console.log(err);
-    res.sendStatus(500);
-  })
+    .then((user: User) => {
+      if (!user) res.status(403).end();
+      const session_key: string = "CHANGE_THIS_TO_BE_GENERATED";
+      repo.setSession(user.getId(), session_key)
+        .then(() => res.cookie('UID', user.getId()).cookie('SID', session_key).send(user).status(200).end())
+    })
+    .catch((err: any) => {
+      console.log(err);
+      res.sendStatus(500);
+    })
 });
 
 // POST /user/logout      Logout an user.
@@ -102,21 +102,21 @@ app.post(`${SERVICE}/logout`, (req: LogoutUserRequest, res: LogoutUserResponse) 
   const session_key = req.cookies.SID;
 
   repo.clearSession(id, session_key)
-  .then(() => res.sendStatus(200))
-  .catch((err: any) => {
-    console.log(err);
-    res.sendStatus(500);
-  })
+    .then(() => res.sendStatus(200))
+    .catch((err: any) => {
+      console.log(err);
+      res.sendStatus(500);
+    })
 });
 
 // GET  /user/profile/:id   Get the profile for the given user id.
 app.get(`${SERVICE}/profile/:id`, (req: GetUserProfileRequest, res: GetUserProfileResponse) => {
   repo.getProfile(req.params.id)
-  .then((profile: Profile) => res.send(profile).status(200).end())
-  .catch((err: any) => {
-    console.log(err);
-    res.sendStatus(500);
-  });
+    .then((profile: Profile) => res.send(profile).status(200).end())
+    .catch((err: any) => {
+      console.log(err);
+      res.sendStatus(500);
+    });
 });
 
 // POST /user/profile/:id   Edit the profile for the given user id.
@@ -126,9 +126,9 @@ app.post(`${SERVICE}/profile/:id`, (req: UpdateUserProfileRequest, res: UpdateUs
   const image_url = req.body.image_url;
 
   repo.editProfile(req.params.id, name, description, image_url)
-  .then((profile: Profile) => res.send(profile).status(200).end())
-  .catch((err: any) => {
-    console.log(err);
-    res.sendStatus(500);
-  });
+    .then((profile: Profile) => res.send(profile).status(200).end())
+    .catch((err: any) => {
+      console.log(err);
+      res.sendStatus(500);
+    });
 });
